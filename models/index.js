46e16ec4cfbb524a1b7e6763c97340622188217e
James S <jamests1000@gmail.com>
2023-01-29 15:05:28 -0500

    added index.js model assoc

--- models/index.js
+++ models/index.js
@@ -5,12 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey: 'ProductTag'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: 'ProductTag'
+});
 
 module.exports = {
   Product,
